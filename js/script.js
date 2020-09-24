89d52709e62d5133f529180c31246409bfafce1d
DiegoMM <dmirandamor@gmail.com>
2020-09-23 23:54:25 -0300

    avance JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     $(".boton_detalles").click(function() {
         alert("Sitio web en construcción, por favor intente más tarde.");
     });
@@ -8,16 +7,25 @@ $(document).ready(function() {
 var slideIndex = 0;
 showSlides();
 
-function showSlides() {
-  var i;
+function showSlides() { /*Pasa las imagenes del carrusel*/
   var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
+  for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4500); // Change image every 2 seconds
+  setTimeout(showSlides, 4500); 
 }
 
+function buscar(){
+  var busqueda = document.getElementById('txt_busqueda').value;
+  /*Esto debiese tomar las palabras y buscarlas en alguna base de datos, pero aún no hay base de datos. */
+}
+
+/* Se supone que esto debiese abrir lo que será la ventana para iniciar sesión. */
+var modal = document.getElementById('modal_usuario');
+function miUsuario(){
+  modal.style.display= block;
+}
 
