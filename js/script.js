6c1d2f5290f3a0a844310bd18e18c6db5b9aa1c0
Fazt <joacoxnaveillanrgmail.com>
2020-09-25 19:46:37 -0300

    git commit -m

--- js/script.js
+++ js/script.js
@@ -1,38 +1,28 @@
-$(document).ready(function() {
-    $(".boton_detalles").click(function() {
-        alert("Sitio web en construcción, por favor intente más tarde.");
-    });
+var slideIndex = 1;
+showSlides(slideIndex);
 
-    $("#usuario").click(function(){
-      
-      var modal = document.getElementById("modal_usuario");
-      modal.style.display = block;
-    });
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-});
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() { /*Pasa las imagenes del carrusel*/
+function showSlides(n) {
+  var i;
   var slides = document.getElementsByClassName("mySlides");
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4500); 
+  dots[slideIndex-1].className += " active";
 }
-
-function buscar(){
-  var busqueda = document.getElementById('txt_busqueda').value;
-  /*Esto debiese tomar las palabras y buscarlas en alguna base de datos, pero aún no hay base de datos. */
-}
-
-/* Se supone que esto debiese abrir lo que será la ventana para iniciar sesión. 
-var modal = document.getElementById('modal_usuario');
-function miUsuario(){
-  modal.style.display= block;
-}*/
-
