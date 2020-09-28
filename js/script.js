b4d4dc35d9ef038643ab01e4cf45af915837b0ec
DiegoMM <dmirandamor@gmail.com>
2020-09-27 22:25:41 -0300

    Pagina de 'Quienes somos', pero antes de unir con contacto

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
+
+$(document).ready(function() {
+  $(".boton_detalles").click(function() {
+      alert("Sitio web en construcción, por favor intente más tarde.");
+  });
+});
+
 var slideIndex = 1;
 showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
 function showSlides(n) {
   var i;
@@ -25,4 +22,53 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+// Controles para imagen siguiente y anterior con flechas:
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Permite cambiar imagenes clicando los puntitos: 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function buscar(){
+  var busqueda = document.getElementById('txt_busqueda').value;
+  /*Esto debiese tomar las palabras y buscarlas en alguna base de datos, pero aún no hay base de datos. */
+}
+
+/* Estas dos funciones de abajo abren y cierran la ventana para iniciar sesión. */
+function miUsuario(){
+  var modal = document.getElementById('modal_usuario');
+  if(modal.style.display == "none" || modal.style.display == ""){
+    modal.style.display= "block";
+  }
+  else{
+    modal.style.display = "none";
+  }
+  
+}
+
+function cerrarModal(){
+  var modal = document.getElementById("modal_usuario");
+  modal.style.display = "none";
+}
+
+function logIn(){
+  var username = document.getElementById("username").value;
+  var pass = document.getElementById("password").value;
+
+  var usuariosRegistrados = 15;
+  var encontrado = false;
+
+  //Aquí se debería contrastar las credenciales con las presentes en la base de datos de los usuarios registrados.
+  for(var i = 0; i<usuariosRegistrados; i++){
+    if(encontrado ==true){
+      alert("Ahora se iniciará sesión!");
+    }
+  }
+  alert("Las credenciales de usuario no son válidas. Intente de nuevo.")
+  encontrado = false;
 }
